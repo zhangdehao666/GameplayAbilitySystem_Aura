cdd4caade8b70cd980b22baf9e387773b15dc7ec
zhangdehao666 <1425621777@qq.com>
2024-09-17 15:51:36 +0800

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright DeHao Zhang
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright DeHao Zhang
+
+#pragma once
+
+#include "CoreMinimal.h"
+
