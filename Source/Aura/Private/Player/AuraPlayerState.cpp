38640b96acc2f9a3be9f6cfdd117df05d89776b3
zhangdehao666 <1425621777@qq.com>
2024-09-18 21:18:15 +0800

    Constructing the Ability System Component and Attribute Set

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,8 +3,21 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
 	// 服务器尝试更新客户端的频率
 	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
