72e7dd790e648046cc29e381de30a20831ca130b
zhangdehao666 <1425621777@qq.com>
2024-09-23 12:40:19 +0800

    Gameplay Effect Delegates

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -31,5 +31,6 @@ protected:
 
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
-	
+
+	virtual void InitAbilityActorInfo();
 };
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -23,5 +23,5 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
+	virtual void InitAbilityActorInfo() override;
 };
