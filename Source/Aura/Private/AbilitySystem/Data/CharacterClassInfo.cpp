d037b4aa1931b541100a07247ad32f06dbb39024
zhangdehao666 <1425621777@qq.com>
2024-09-26 14:44:59 +0800

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright DeHao Zhang
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
