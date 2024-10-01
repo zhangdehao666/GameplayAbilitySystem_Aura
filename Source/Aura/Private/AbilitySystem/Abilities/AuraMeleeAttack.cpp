8f6a97040b2d7c781c30f66349db2e184862be16
zhangdehao666 <1425621777@qq.com>
2024-10-01 14:43:25 +0800

    Melee Attack Ability

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraMeleeAttack.cpp
@@ -0,0 +1,5 @@
+// Copyright DeHao Zhang
+
+
+#include "AbilitySystem/Abilities/AuraMeleeAttack.h"
+
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -50,6 +50,8 @@ struct FAuraGameplayTags
 	FGameplayTag Damage_Arcane;
 	FGameplayTag Damage_Physical;
 
+	FGameplayTag Abilities_Attack;
+
 	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
 
 	FGameplayTag Effects_HitReact;
