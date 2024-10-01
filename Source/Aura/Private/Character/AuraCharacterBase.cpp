a922f18f676185b6de7d78f4e3996bc2559553ab
zhangdehao666 <1425621777@qq.com>
2024-10-01 22:14:53 +0800

    Tagged Montage

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -76,6 +76,11 @@ AActor* AAuraCharacterBase::GetAvatar_Implementation()
 	return this;
 }
 
+TArray<FTaggedMontage> AAuraCharacterBase::GetAttackMontages_Implementation()
+{
+	return AttackMontages;
+}
+
 void AAuraCharacterBase::InitAbilityActorInfo()
 {
 }
