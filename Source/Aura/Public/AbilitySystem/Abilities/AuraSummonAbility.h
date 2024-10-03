7899576089ab34faec7f8517e107f9a1b41b687b
zhangdehao666 <1425621777@qq.com>
2024-10-03 21:20:57 +0800

    Select Minion Class at Random

--- Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -17,6 +17,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	TArray<FVector> GetSpawnLocations();
 
+	UFUNCTION(BlueprintPure, Category = "Summoning")
+	TSubclassOf<APawn> GetRandomMinionClass();
+
 	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
 	int32 NumMinions = 5;
 
