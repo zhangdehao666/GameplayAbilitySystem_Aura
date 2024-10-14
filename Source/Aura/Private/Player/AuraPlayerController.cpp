3cc5d9410a9f3976aff60e1570f5c70eb67373cc
zhangdehao666 <1425621777@qq.com>
2024-10-14 20:26:57 +0800

    Ignore Enemies while Magic Circle Active

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -12,6 +12,7 @@
 #include "NiagaraFunctionLibrary.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Actor/MagicCircle.h"
+#include "Aura/Aura.h"
 #include "Components/DecalComponent.h"
 #include "Components/SplineComponent.h"
 #include "Input/AuraInputComponent.h"
@@ -129,7 +130,8 @@ void AAuraPlayerController::CursorTrace()
 		LastActor = ThisActor = nullptr;
 		return;
 	}
-	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	const ECollisionChannel TraceChannel = IsValid(MagicCircle) ? ECC_ExcludePlayers : ECC_Visibility;
+	GetHitResultUnderCursor(TraceChannel, false, CursorHit);
 	if (!CursorHit.bBlockingHit) return;
 	LastActor = ThisActor;
 	ThisActor = CursorHit.GetActor();
