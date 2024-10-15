b81f8bdd5d78291298d56a0354d4a52a6f31dee7
zhangdehao666 <1425621777@qq.com>
2024-10-15 22:33:32 +0800

    Load Slot View Model

---
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -0,0 +1,17 @@
+// Copyright DeHao Zhang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
