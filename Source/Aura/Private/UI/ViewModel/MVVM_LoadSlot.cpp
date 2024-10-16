fa09a9d1086c7fbfa5e8750b233756894a30a01a
zhangdehao666 <1425621777@qq.com>
2024-10-16 09:35:53 +0800

    Binding Variables to ViewModels

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
@@ -8,3 +8,9 @@ void UMVVM_LoadSlot::InitializeSlot()
 	// TODO: Check slot status based on loaded data
 	SetWidgetSwitcherIndex.Broadcast(2);
 }
+
+void UMVVM_LoadSlot::SetPlayerName(FString InPlayerName)
+{
+	// 会报错，不影响运行
+	UE_MVVM_SET_PROPERTY_VALUE(PlayerName, InPlayerName);
+}
